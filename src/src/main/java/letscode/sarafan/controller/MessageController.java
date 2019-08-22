b01e644d0fce7123e15b1847e8ca1e74a5a82c3c
Михаил Катаев <kataev.06@hotmail.com>
2019-08-22 22:35:14 +0300

    add new methods to Message Controller

--- src/src/main/java/letscode/sarafan/controller/MessageController.java
+++ src/src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,10 +1,7 @@
 package letscode.sarafan.controller;
 
 import letscode.sarafan.exceptions.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,23 +12,64 @@ import java.util.Map;
 @RequestMapping("message")
 public class MessageController {
 
-    public List<Map<String, String>> message =
-            new ArrayList<Map<String, String>>(){{
-                add(new HashMap<String, String>() {{ put("id", "1");put("text", "First message"); }});
-                add(new HashMap<String, String>() {{ put("id", "2");put("text", "Second message"); }});
-                add(new HashMap<String, String>() {{ put("id", "3");put("text", "Third message"); }});
+    private int counter = 4;
+
+    private List<Map<String, String>> messages =
+            new ArrayList<Map<String, String>>() {{
+                add(new HashMap<String, String>() {{
+                    put("id", "1");
+                    put("text", "First message");
+                }});
+                add(new HashMap<String, String>() {{
+                    put("id", "2");
+                    put("text", "Second message");
+                }});
+                add(new HashMap<String, String>() {{
+                    put("id", "3");
+                    put("text", "Third message");
+                }});
             }};
 
     @GetMapping
     public List<Map<String, String>> list() {
-        return message;
+        return messages;
     }
 
     @GetMapping("{id}")
     public Map<String, String> getOne(@PathVariable String id) {
-        return message.stream()
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+
+        messages.add(message);
+
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(message.get("id"));
+
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+
+        return messageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+
+        messages.remove(message);
     }
 }

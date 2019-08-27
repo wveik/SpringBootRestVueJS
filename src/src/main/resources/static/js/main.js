be3eb3174881c7c5e23534bf643a330bae5f9f93
wveik <kataev_06@list.ru>
2019-08-27 10:35:21 +0300

    add Ajax request to REST api

--- src/src/main/resources/static/js/main.js
+++ src/src/main/resources/static/js/main.js
@@ -1,6 +1,31 @@
+
+var messageApi = Vue.resource('message{/id}')
+
+Vue.component('message-row', {
+    props: ['message'],
+    template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
+});
+
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div>' +
+        '<message-row v-for="message in messages" :message="message" :key="message.id" />'
+                + '</div>',
+    created: function () {
+        messageApi.get().then(result =>
+            result.json().then( data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
+});
+
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages" />',
     data: {
-        message: 'Привет, Vue!'
+        messages: []
     }
 });
